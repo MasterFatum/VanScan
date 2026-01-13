923efcbb77bc9130c81a05d7de5fc4970658f7b4
VAN <i.panfilov@fckrasnodar.ru>
2026-01-13 10:21:44 +0300

    Add scan select ports

--- main.py
+++ main.py
@@ -2,6 +2,22 @@ import socket
 from datetime import datetime
 import ipaddress
 
+def get_service_name(port):
+    services = {
+        20: "FTP-DATA", 21: "FTP", 22: "SSH", 23: "Telnet",
+        25: "SMTP", 43: "WHOIS", 53: "DNS", 80: "HTTP",
+        115: "SFTP", 123: "NTP", 143: "IMAP", 161: "SNMP",
+        179: "BGP", 443: "HTTPS", 445: "MICROSOFT-DS",
+        514: "SYSLOG", 515: "PRINTER", 993: "IMAPS",
+        995: "POP3S", 1080: "SOCKS", 1194: "OpenVPN",
+        1433: "SQL Server", 1723: "PPTP", 3128: "HTTP",
+        3268: "LDAP", 3306: "MySQL", 3389: "RDP",
+        5432: "PostgreSQL", 5900: "VNC", 8080: "Tomcat",
+        10000: "Webmin"
+    }
+    return services.get(port, "Unknown")
+
+
 def scan_port(host, ports):
     start = datetime.now().replace(microsecond=0)
     open_ports = []
@@ -33,22 +49,20 @@ def scan_port(host, ports):
     print(f"<Time: {ends - start}>")
     return open_ports
 
-def get_service_name(port):
-    services = {
-        20: "FTP-DATA", 21: "FTP", 22: "SSH", 23: "Telnet",
-        25: "SMTP", 43: "WHOIS", 53: "DNS", 80: "HTTP",
-        115: "SFTP", 123: "NTP", 143: "IMAP", 161: "SNMP",
-        179: "BGP", 443: "HTTPS", 445: "MICROSOFT-DS",
-        514: "SYSLOG", 515: "PRINTER", 993: "IMAPS",
-        995: "POP3S", 1080: "SOCKS", 1194: "OpenVPN",
-        1433: "SQL Server", 1723: "PPTP", 3128: "HTTP",
-        3268: "LDAP", 3306: "MySQL", 3389: "RDP",
-        5432: "PostgreSQL", 5900: "VNC", 8080: "Tomcat",
-        10000: "Webmin"
-    }
-    return services.get(port, "Unknown")
 
 def scan_ip_range(ip_range, port_list):
+    try:
+        network = ipaddress.IPv4Network(ip_range, strict=False)
+        print(f"\nScanning network: {network}\n")
+
+        for ip in network.hosts():
+            scan_port(str(ip), port_list)
+            print("-" * 50)
+    except Exception as e:
+        print(f"Invalid IP range: {e}")
+
+
+def scan_ip_range_selected_ports(ip_range, port_list):
     try:
         network = ipaddress.IPv4Network(ip_range, strict=False)
         print(f"\nScanning network: {network}\n")
@@ -66,18 +80,29 @@ def main():
              179, 443, 445, 514, 515, 993, 995, 1080, 1194,
              1433, 1723, 3128, 3268, 3306, 3389, 5432, 5900, 8080, 10000]
 
-    choice = input("Choose mode:\n1 - Scan single host\n2 - Scan IP range (e.g. 192.168.1.0/24)\n3 - Exit\n> ")
+    choice = input("Choose mode:\n1 - Scan single host (ALL PORTS)\n2 - Scan IP range (ALL PORTS)\n3 - Scan IP range (SELECTED PORT)\n4 - Exit\n> ")
 
     match choice:
         case "1":
-            host = input('Enter the host name or IP address: ')
+            host = input('Enter the host name or IP address (ALL PORTS): ')
             if host:
                 scan_port(host, ports)
 
         case "2":
-            ip_range = input("Enter IP range (CIDR format, e.g. 192.168.1.0/24): ")
+            ip_range = input("Enter IP range (CIDR format, EXAMPLE: 192.168.1.0/24): ")
             scan_ip_range(ip_range, ports)
         case "3":
+            ip_range = input("Enter IP range (CIDR format, EXAMPLE: 192.168.1.0/24): ")
+            ports_input = input("Enter ports (EXAMPLE: 22, 23, 3306): ")
+            try:
+                selected_ports = [int(port.strip()) for port in ports_input.split(',') if port.strip().isdigit()]
+                if not selected_ports:
+                    print("No valid ports entered.")
+                else:
+                    scan_ip_range_selected_ports(ip_range, selected_ports)
+            except ValueError as e:
+                print("Invalid port input. Please enter numeric values separated by commas.")
+        case "4":
             exit(0)
 
 
